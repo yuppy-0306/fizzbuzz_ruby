5bf0c41890fd980c89ff5de1f8a757ac3b4b578d
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-13 11:17:39 +0900

    3,5,15分割

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,5 +1,13 @@
 (1..20).each do |i|
-    puts i
+    if i % 3 == 0 && i % 5 == 0
+        puts "FizzBuzz"
+    elsif i % 3 == 0
+        puts "Fizz"
+    elsif i % 5 == 0
+        puts "Buzz"
+    else
+        puts i
+    end
 end
 
 #3,6,6,12,15,18-Fizz
