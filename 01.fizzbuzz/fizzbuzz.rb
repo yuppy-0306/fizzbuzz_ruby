06d444e95300b759506868c9e8bff583669551b3
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-19 10:04:39 +0900

    修正（再提出）

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,18 +1,14 @@
 (1..20).each do |i|
-    fizzbuzzz_num = 15
-    fizz_num = 3
-    buzz_num = 5
-    if i % fizzbuzzz_num == 0 
-        puts "FizzBuzz"
-    elsif i % fizz_num == 0
-        puts "Fizz"
-    elsif i % buzz_num == 0
-        puts "Buzz"
-    else
-        puts i
-    end
+  fizzbuzz_num = 15
+  fizz_num = 3
+  buzz_num = 5
+  if i % fizzbuzz_num == 0
+    puts 'FizzBuzz'
+  elsif i % fizz_num == 0
+    puts 'Fizz'
+  elsif i % buzz_num == 0
+    puts 'Buzz'
+  else
+    puts i
+  end
 end
-
-#3,6,6,12,15,18-Fizz
-#5,10,15-Buzz
-#15-FizzBuzz
