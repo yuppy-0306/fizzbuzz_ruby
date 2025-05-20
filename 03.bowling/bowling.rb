6df037a06b4387d2246c05adf77b3c1dbbd55b54
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-20 13:19:27 +0900

    旧ルール条件

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,31 +1,56 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'debug'
+
 score = ARGV[0]
 scores = score.split(',')
+
 shots = []
 scores.each do |s|
   if s == 'X'
     shots << 10
-    shots << 0
   else
     shots << s.to_i
   end
 end
 
 frames = []
-shots.each_slice(2) do |s|
-  frames << s
+i = 0
+while frames.size < 10
+  if shots[i] == 10 && frames.size < 9
+    frames << [10]
+    i += 1
+  else
+    frames << [shots[i], shots[i + 1]]
+    i += 2
+  end
 end
 
+
 point = 0
-frames.each do |frame|
-  point += if frame[0] == 10
-             30
-           elsif frame.sum == 10
-             frame[0] + 10
-           else
-             frame.sum
-           end
+idx = 0
+frames.each_with_index do |frame, idx|
+  if idx < 9
+
+    if frame[0] == 10
+      next_frame = frames[idx + 1]
+      if next_frame[0] == 10 && idx < 8
+        bonus = 10 + frames[idx + 2][0]
+      else
+        bonus = next_frame[0] + next_frame[1] 
+      end
+      point += 10 + bonus
+    
+    elsif frame.sum == 10
+      point += 10 + frames[idx + 1][0]
+    else
+      point += frame.sum
+    end
+  else
+    
+    point += shots[i - 2, 3].compact.sum
+  end
 end
-p point
+
+puts point
