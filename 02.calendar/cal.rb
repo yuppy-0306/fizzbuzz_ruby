8ab7d82c9867b8c10cfee254cce9c1199563cf2f
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-21 11:55:35 +0900

    rubocop修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,28 +1,29 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'date'
 require 'optparse'
 
-def display_calendar(year,month)
-    first_day = Date.new(year, month, 1)
-    last_day = Date.new(year,month,-1)
-    printf "       #{month}月 #{year}\n"
-    printf " 日 月 火 水 木 金 土\n"
-    first_day_offset = first_day.wday
-    print "   " * first_day_offset
+def display_calendar(year, month)
+  first_day = Date.new(year, month, 1)
+  last_day = Date.new(year, month, -1)
+  printf "       #{month}月 #{year}\n"
+  printf " 日 月 火 水 木 金 土\n"
+  first_day_offset = first_day.wday
+  print '   ' * first_day_offset
 
-    (first_day..last_day).each do |date|
-        print date.day.to_s.rjust(3)
-        puts if date.saturday?
-    end
-    puts
+  (first_day..last_day).each do |date|
+    print date.day.to_s.rjust(3)
+    puts if date.saturday?
+  end
+  puts
 end
-
 
 options = {}
 opt = OptionParser.new
 
-opt.on("-y YEAR", Integer) { |y| options[:year] = y }
-opt.on("-m MONTH", Integer) { |m| options[:month] = m }
+opt.on('-y YEAR', Integer) { |y| options[:year] = y }
+opt.on('-m MONTH', Integer) { |m| options[:month] = m }
 opt.parse!
 
 year = options[:year] || Date.today.year
@@ -30,15 +31,13 @@ month = options[:month] || Date.today.month
 
 display_calendar(year, month)
 
+# 今月のカレンダーを表示するプログラム
+# -yで年を指定
+# -mで月を指定
+# デフォルトは今年の今月のカレンダーが表示される
+# 1970年から2100年までのカレンダーを表示する
+# 一週間ごとに改行
 
-
-#今月のカレンダーを表示するプログラム
-#-yで年を指定
-#-mで月を指定
-#デフォルトは今年の今月のカレンダーが表示される
-#1970年から2100年までのカレンダーを表示する
-#一週間ごとに改行
-
-#指定された年月の1日から月末まで順番に処理する。
-#各日付が週の何番目の日かを把握し、適切な位置に表示する。
-#週の区切りで改行する。
+# 指定された年月の1日から月末まで順番に処理する。
+# 各日付が週の何番目の日かを把握し、適切な位置に表示する。
+# 週の区切りで改行する。
