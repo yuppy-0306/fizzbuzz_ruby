f71e8ba8891aeffc2ed14cb0b76b7b3bdfb53f8b
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-30 15:35:49 +0900

    取得したエントリのフィルタリング

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,7 +1,21 @@
 #!/usr/bin/env ruby
-current_directory = Dir.pwd
 
-children = Dir.children(current_directory)
-children.each do |child|
-  puts child
+current_directory = Dir.pwd
+all_entries = Dir.children(current_directory)
+
+def filter_entries(entries)
+  entries.reject do |entry_name|
+    entry_name == "." ||         
+    entry_name == ".." ||        
+    entry_name.start_with?(".")  
+  end
 end
+
+visible_entries = filter_entries(all_entries)
+visible_entries.each do |entry|
+  puts entry
+end
+
+
+
+
