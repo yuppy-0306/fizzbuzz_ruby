fb79bf0a8da4cbb30a593e00e3abcb4893b117dc
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-06-02 10:29:57 +0900

    表示可能列数、行数計算

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -23,7 +23,16 @@ def entries_length_width(entries)
     return 0 if entries.empty?
     max_length = entries.map(&:length).max
     max_entries_width = max_length + 2
-  return max_entries_width_width
+  return max_entries_width
 end
 
 TERMINAL_WIDTH = 80
+
+def get_grid_dims (entries, terminal_width, max_entries_width)
+  return { num_columns: 0, num_rows: 0 } if entries.empty?
+  max_entries_width = [1,max_entries_width].max
+  num_columns = (terminal_width/max_entries_width).floor
+  num_columns = [1,num_columns].max
+  num_rows = (entries.size.to_f / num_columns).ceil
+  { num_columns: num_columns,num_rows: num_rows}
+end
