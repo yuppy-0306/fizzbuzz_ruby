a5d689fdd21323b2c728d19f9e256d3d4f8a486e
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-06-09 11:00:25 +0900

    Hashを配列に変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -22,13 +22,13 @@ end
 TERMINAL_WIDTH = 80
 
 def get_grid_dims(entries, terminal_width, max_entries_width)
-  return { num_columns: 0, num_rows: 0 } if entries.empty?
+  return [0, 0] if entries.empty?
 
   max_entries_width = [1, max_entries_width].max
   num_columns = (terminal_width / max_entries_width).floor
   num_columns = [1, num_columns].max
   num_rows = (entries.size.to_f / num_columns).ceil
-  { num_columns: num_columns, num_rows: num_rows }
+  [num_columns, num_rows]
 end
 
 def build_grid(entries, num_rows, num_columns)
@@ -46,12 +46,11 @@ def print_grid(entries, terminal_width)
 
   sorted = entries.sort
   width = entries_length_width(sorted)
-  dims = get_grid_dims(sorted, terminal_width, width)
-  grid = build_grid(sorted, dims[:num_rows], dims[:num_columns])
+  num_columns, num_rows = get_grid_dims(sorted, terminal_width, width)
+  grid = build_grid(sorted, num_rows, num_columns)
 
   grid.each do |row|
-    row.each { |entry| print (entry || '').ljust(width) }
-    puts
+    puts row.map { |entry| (entry || '').ljust(width) }.join
   end
 end
 
