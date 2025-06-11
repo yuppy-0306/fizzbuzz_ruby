0ebe63d6648b5cdd6d24b58b8a903ea257abf4fb
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-06-11 15:32:42 +0900

    スコア表示

--- 06.bowling_object/bowling.rb
+++ 06.bowling_object/bowling.rb
@@ -1,43 +1,9 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-score = ARGV[0]
-scores = score.split(',')
+require_relative 'game'
 
-shots = scores.map { |s| s == 'X' ? 10 : s.to_i }
+score_string = ARGV[0]
 
-frames = []
-i = 0
-while frames.size < 9
-  if shots[i] == 10
-    frames << [10]
-    i += 1
-  else
-    frames << [shots[i], shots[i + 1]]
-    i += 2
-  end
-end
-frames << shots[i..]
-
-point = 0
-
-frames.each_with_index do |frame, idx|
-  
-
-  point += frame.sum
-
-  next if idx >= 9 || frame.sum < 10
-
-  point += frames[idx + 1][0]
-  next if frame[0] != 10
-
-  point += if frames[idx + 1][0] == 10 && idx < 8
-             frames[idx + 2][0]
-           else
-             frames[idx + 1][1] || 0
-           end
-end
-
-
-
-puts point
+game = Game.new(score_string)
+puts game.calculate_score
