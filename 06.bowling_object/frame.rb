341e31746ba6ae5d41bcaadeb8bfa48cd64e5be5
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-06-11 15:29:10 +0900

    スコア計算

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class Frame
-    attr_reader :shots
+  attr_reader :shots
 
   def initialize(shots)
     @shots = shots
@@ -20,5 +20,22 @@ class Frame
 
   def open?
     total_pins < 10
+  end
+
+  def score(frames, idx)
+    base = total_pins
+    return base if idx >= 9
+    if strike?
+      base + next_pins(frames, idx, 2)
+    elsif spare?
+      base + next_pins(frames, idx, 1)
+    else
+      base
+    end
+  end
+
+  def next_pins(frames, idx, n)
+    shots = frames[(idx + 1)..].flat_map(&:shots)
+    shots.first(n).sum(&:pins)
   end
 end
--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -2,4 +2,34 @@ require_relative 'frame'
 require_relative 'shot'
 
 class Game
+  def initialize(score_string)
+    @shots = parse_shots(score_string)
+    @frames = build_frames(@shots)
+  end
+
+  def calculate_score
+    @frames.each_with_index.sum { |frame, idx| frame.score(@frames, idx) }
+  end
+
+  private
+
+  def parse_shots(score_string)
+    score_string.split(',').map { |s| Shot.new(s == 'X' ? 10 : s.to_i) }
+  end
+
+  def build_frames(shots)
+    frames = []
+    i = 0
+    9.times do
+      if shots[i].strike?
+        frames << Frame.new([shots[i]])
+        i += 1
+      else
+        frames << Frame.new([shots[i], shots[i + 1]])
+        i += 2
+      end
+    end
+    frames << Frame.new(shots[i..])
+    frames
+  end
 end
