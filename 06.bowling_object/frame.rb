767fce29fe98b5633a33ac4a9bc5858ef3cfe38f
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-06-11 15:11:11 +0900

    フレーム判断

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -1,3 +1,24 @@
 # frozen_string_literal: true
 class Frame
+    attr_reader :shots
+
+  def initialize(shots)
+    @shots = shots
+  end
+
+  def total_pins
+    @shots.sum(&:pins)
+  end
+
+  def strike?
+    @shots.any?(&:strike?) && @shots.size == 1
+  end
+
+  def spare?
+    !strike? && total_pins == 10
+  end
+
+  def open?
+    total_pins < 10
+  end
 end
